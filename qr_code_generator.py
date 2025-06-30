4a50dff6f3382cbfb90b64f0d7eeb1ff3b1ee510
PurpleLightSpeed <lotaagulue@gmail.com>
2025-06-30 14:02:23 -0400

    gitignore for virtual environment added qr code generator script sample image file for testing

---
+++ qr_code_generator.py
@@ -0,0 +1,14 @@
+import qrcode
+
+data = input("Enter the text or URL: ").strip()
+filename = input("Enter the filename (with .png extension): ").strip()
+qr = qrcode.QRCode(box_size=10, border=4)
+qr.add_data(data)
+qr.make(fit=True)  # Make the QR code before creating the image
+image = qr.make_image(fill_color="black", back_color="white")
+image.save(filename)
+print(f"QR code saved as {filename}")
+
+
+# In Terminal: source venv/bin/activate
+# Run: python qr_code_generator.py
