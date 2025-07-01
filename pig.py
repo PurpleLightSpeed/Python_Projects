1341ed3383a59bd5b29f0cf6b59a8274681dcdea
PurpleLightSpeed <lotaagulue@gmail.com>
2025-07-01 13:02:01 -0400

    no comment

---
+++ pig.py
@@ -0,0 +1,53 @@
+# multiplayer game
+# everyone gets to role a die
+# when you role a die you get a number thats added to your total score
+# you can role as many times as you want until you end your turn
+# if you role a one you lose all your points in that turn and its moved to the next player
+# first player to 50 or any number set wins
+
+import random
+
+def roll():
+    roll = random.randint(1,6)
+    return roll
+
+while True:
+    players = input("Enter the number of players (2-4): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 - 4 players.")
+    else:
+        print("Invalid, try again.")
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+    
+    for player_id in range(players):
+        print(f"\nPlayer number {player_id + 1} turn has just started!")
+        print(f"Your total score is: {player_scores[player_id]}\n")
+        current_score = 0
+        
+        while True:
+            should_roll = input("Would you like to roll (y)? ").lower()
+            if should_roll != "y":
+                break
+            
+            value = roll()
+            
+            if value == 1:
+                print(f"You rolled a: {value}! Turn over!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print(f"You rolled a: {value}")
+            
+            print(f"Your score is: {current_score}!")
+        
+        player_scores[player_id] += current_score
+        print(f"Your total score is: {player_scores[player_id]}")
