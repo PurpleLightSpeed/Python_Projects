6167d140dbbf6f13b8603846956eb25c00d365e2
PurpleLightSpeed <lotaagulue@gmail.com>
2025-07-03 20:01:57 -0400

    updated madlibs_generator.py

--- madlibs_generator.py
+++ madlibs_generator.py
@@ -1,7 +1,7 @@
 with open("madlibs_story.txt", "r") as f:
     story = f.read()
     
-words = []
+words = set()
 start_of_word = -1
 
 target_start = "<"
@@ -12,4 +12,17 @@ for i, char in enumerate(story):
         start_of_word = i
         
     if char == target_end and start_of_word != -1:
-        words = story[start_of_word: i + 1]
+        word = story[start_of_word: i + 1]
+        words.add(word)
+        start_of_word = -1
+        
+answers = {}
+
+for word in words:
+    answer = input(f"Enter a word for {word} : ")
+    answers[word] = answer
+    
+for word in words:
+    story = story.replace(word, answers[word])
+    
+print(story)
